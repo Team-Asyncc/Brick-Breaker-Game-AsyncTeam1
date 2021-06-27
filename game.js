bfd9a81d4aeb7233c7781522b11be1c891dc749d
Harsh <harshmkvviv@gmail.com>
2021-06-28 02:50:08 +0530

    confetti added

--- game.js
+++ game.js
@@ -379,10 +379,85 @@ restart.addEventListener("click", function () {
 function showYouWin() {
   gameover.style.display = "block";
   youwon.style.display = "block";
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
 }
 
 // SHOW YOU LOSE
 function showYouLose() {
   gameover.style.display = "block";
   youlose.style.display = "block";
+  snowfall();
 }
+
+// confetti add
+var count = 200;
+var defaults = {
+  origin: { y: 0.7 }
+};
+
+function fire(particleRatio, opts) {
+  confetti(Object.assign({}, defaults, opts, {
+    particleCount: Math.floor(count * particleRatio)
+  }));
+}
+
+// snowfall confetti
+
+function snowfall(){
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var skew = 1;
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+  var timeLeft = animationEnd - Date.now();
+  var ticks = Math.max(200, 500 * (timeLeft / duration));
+  skew = Math.max(0.8, skew - 0.001);
+
+  confetti({
+    particleCount: 1,
+    startVelocity: 0,
+    ticks: ticks,
+    origin: {
+      x: Math.random(),
+      // since particles fall down, skew start toward the top
+      y: (Math.random() * skew) - 0.2
+    },
+    colors: ['#ffffff'],
+    shapes: ['circle'],
+    gravity: randomInRange(0.4, 0.6),
+    scalar: randomInRange(0.4, 1),
+    drift: randomInRange(-0.4, 0.4)
+  });
+
+  if (timeLeft > 0) {
+    requestAnimationFrame(frame);
+  }
+  }());
+}
+
+
